34fa2bbaa0b6023c9f66a6d1ed1d9bd2b4e3a6d7
Devoloper account <dev@minimal.localdomain>
2017-11-02 14:12:47 +0300

    change

--- main.cpp
+++ main.cpp
@@ -1,44 +1,28 @@
 #include <iostream>
 
 using namespace std;
-
-class Base
+int main()
 {
-public:
-    Base() { cout << "Construct Base object\n"; }
-    ~Base() { cout << "Destruct Base object\n"; }
-
-    void func() const { cout << "Function func() of class Base\n"; }
-};
-
-class Child: public Base
-{
-public:
-    Child() { cout << "Construct Child object\n"; }
-    ~Child() { cout << "Destruct Child object\n"; }
-
-    void func() const { cout << "Function func() of class Child\n"; }
-};
-
-void Function(const Base &obj)
-{
-    cout << "* Call method func() for object obj" << endl;
-    obj.func();
-}
-
-int main(int argc, char *argv[])
-{
-    cout << "* Create Object base" << endl;
-    Base base;
-
-    cout << "* Create Object child" << endl;
-    Child child;
-
-    cout << "* Call method func() for object base" << endl;
-    Function(base);
-
-    cout << "* Call method func() for object child" << endl;
-    Function(child);
-
+    float a[100];
+    int n;
+    cout << "n= ";
+    cin >> n;
+    float s=0;
+    for (int i=0; i<n; ++i)
+    {	
+	cout << "a[" << i << "]= ";
+	cin >> a[i];
+	s+=a[i];
+    }
+    if (s/2==0)
+    {
+	s=s-10;
+	cout << "s= " << s << endl;
+    }
+    else
+    {
+	s=s-100;
+        cout << "s= " << s << endl;
+    }
     return 0;
 }
