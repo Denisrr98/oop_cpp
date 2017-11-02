e243fb87ab67e338d9438ce2c876fdcbe55e7244
Devoloper account <dev@minimal.localdomain>
2017-11-02 14:20:44 +0300

    change

--- main.cpp
+++ main.cpp
@@ -1,44 +1,17 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
-
-class Base
+int main()
 {
-public:
-    Base() { cout << "Construct Base object\n"; }
-    ~Base() { cout << "Destruct Base object\n"; }
-
-    virtual void func() const { cout << "Function func() of class Base\n"; }
-};
-
-class Child: public Base
-{
-public:
-    Child() { cout << "Construct Child object\n"; }
-    ~Child() { cout << "Destruct Child object\n"; }
-
-    void func() const { cout << "Function func() of class Child\n"; }
-};
-
-void Function(const Base &obj)
-{
-    cout << "* Call method func() for object obj" << endl;
-    obj.func();
-}
-
-int main(int argc, char *argv[])
-{
-    cout << "* Create Object base" << endl;
-    Base base;
-
-    cout << "* Create Object child" << endl;
-    Child child;
-
-    cout << "* Call method func() for object base" << endl;
-    Function(base);
-
-    cout << "* Call method func() for object child" << endl;
-    Function(child);
-
+    int age;
+    string name, address;
+    cout << "Name: ";
+    getline(cin,name);
+    cout << "Age: ";
+    cin >> age;
+    cout << "Address: ";
+    getline(cin,address)
+    cout << "\n" << name << "\t" << age << "\t" << address << endl;
     return 0;
 }
